c93626d289ab69f348fc7841a3d5fcba3fe981dc
changxinaglin <281532612@qq.com>
2020-12-18 19:49:57 +0800

    add animation

--- 1218/lingame/lin_game.js
+++ 1218/lingame/lin_game.js
@@ -12,10 +12,10 @@ class LinGame {
     // events
     var self = this
     window.addEventListener('keydown', event => {
-      this.keydowns[event.key] = true
+      this.keydowns[event.key] = 'down'
     })
     window.addEventListener('keyup', function(event) {
-      self.keydowns[event.key] = false
+      self.keydowns[event.key] = 'up'
     })
     this.init()
   }
@@ -48,9 +48,14 @@ class LinGame {
     var actions = Object.keys(g.actions)
     for (var i = 0; i < actions.length; i++) {
       var key = actions[i]
-      if (g.keydowns[key]) {
+      var status = g.keydowns[key]
+      if (status == 'down') {
         // 如果按键被按下，调用注册的 action
-        g.actions[key]()
+        g.actions[key]('down')
+      } else if (status == 'up') {
+        g.actions[key]('up')
+        // 删除掉这个 key 的状态
+        g.keydowns[key] = null
       }
     }
     // upate
--- 1218/scene/title/scene_title.js
+++ 1218/scene/title/scene_title.js
@@ -1,104 +1,28 @@
-class LinLabel {
-  constructor(game, text) {
-    this.game = game
-    this.text = text
-  }
-  static new(game, text) {
-    return new this(game, text)
-  }
-  draw() {
-    this.game.context.fillText(this.text, 10, 190)
-  }
-  update() {
-
-  }
-}
-
-class LinParticle extends LinImage {
-  constructor(game) {
-    super(game, 'fire')
-    this.setup()
-  }
-  setup() {
-    this.life = 20
-  }
-  init(x, y, vx, vy) {
-    this.x = x
-    this.y = y
-    this.vx = vx
-    this.vy = vy
-  }
-  update() {
-    this.life--
-    this.x += this.vx
-    this.y += this.vy
-    var factor = 0.02
-    this.vx += factor * this.vx
-    this.vy += factor * this.vy
-  }
-}
-
-class LinParticleSystem {
-  constructor(game) {
-    this.game = game
-    this.setup()
-  }
-  static new(game) {
-    return new this(game)
-  }
-  setup() {
-    this.duration = 50
-    this.x = 150
-    this.y = 200
-    this.numberOfParticles = 100
-    this.particles = []
-  }
-  update() {
-    this.duration--
-    // 添加小火花
-    if (this.particles.length < this.numberOfParticles) {
-      var p = LinParticle.new(this.game)
-      // 设置初始坐标
-      var s = 2
-      var vx = randomBetween(-s, s)
-      var vy = randomBetween(-s, s)
-      p.init(this.x, this.y, vx, vy)
-      this.particles.push(p)
-    }
-    // 更新所有的小火花
-    for (var p of this.particles) {
-      p.update()
-    }
-    // 删除死掉的小火花
-    this.particles = this.particles.filter(p => p.life > 0)
-  }
-  draw() {
-    if (this.duration < 0) {
-      // TODO: 这是一个临时方案
-      // 应该从 scene 中删除自己才对
-      return
-    }
-    for (var p of this.particles) {
-      p.draw()
-    }
-  }
-}
-
 class SceneTitle extends LinScene {
   constructor(game) {
     super(game)
-    var label = LinLabel.new(game, 'hello')
+    var label = LinLabel.new(game, 'hello from lin')
     this.addElements(label)
 
+    // cave bg
+    var cave = LinImage.new(game, 'cave')
+    this.addElements(cave)
+    // player
+    var w = LinAnimation.new(game)
+    w.x = 100
+    w.y = 530
+    this.w = w
+    this.addElements(w)
 
-
-    var ps = LinParticleSystem.new(game)
-    this.addElements(ps)
+    this.setupInputs()
   }
-
-  // draw() {
-  //   super.draw()
-  //   // draw lables
-  //   // this.game.context.fillText('按 k 开始游戏', 10, 190)
-  // }
+  setupInputs() {
+    var self = this
+    self.game.registerAction('a', function(keyStatus) {
+      self.w.move(-2, keyStatus)
+    })
+    self.game.registerAction('d', function(keyStatus) {
+      self.w.move(2, keyStatus)
+    })
+  }
 }
