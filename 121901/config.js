bca983c2ad3a645c947dd4ddc755418a63107f4f
changxinaglin <281532612@qq.com>
2020-12-19 19:02:13 +0800

    config

---
+++ 121901/config.js
@@ -0,0 +1,18 @@
+const config = {
+  pipe_space: {
+    _comment: '2根管子垂直方向的间距',
+    value: 150,
+  },
+  pipe_space1: {
+    _comment: '1 根管子垂直方向的间距',
+    value: 150,
+  },
+  pipe_space2: {
+    _comment: '333 根管子垂直方向的间距',
+    value: 150,
+  },
+  pipe_space3: {
+    _comment: '2333 根管子垂直方向的间距',
+    value: 150,
+  },
+}
---
+++ 121901/scene/title/scene_title.js
@@ -0,0 +1,112 @@
+class Pipes {
+  constructor(game) {
+    this.game = game
+    this.pipes = []
+    this.pipeSpace = 50
+    this.管子横向间距 = 200
+    this.columsOfPipe = 3
+    for (var i = 0; i < this.columsOfPipe; i++) {
+      var p1 = LinImage.new(game, 'pipe')
+      p1.flipY = true
+      p1.x = 500 + i * this.管子横向间距
+      var p2 = LinImage.new(game, 'pipe')
+      p2.x = p1.x
+      this.resetPipesPosition(p1, p2)
+      this.pipes.push(p1)
+      this.pipes.push(p2)
+    }
+  }
+  static new(game) {
+    return new this(game)
+  }
+  resetPipesPosition(p1, p2) {
+    p1.y = randomBetween(-200, 0)
+    p2.y = p1.y + p1.h + this.pipeSpace
+  }
+  update() {
+    for (var p of this.pipes) {
+      p.x -= 5
+      if (p.x < -100) {
+        p.x += this.管子横向间距 * this.columsOfPipe
+      }
+    }
+  }
+  draw() {
+    var context = this.game.context
+    for (var p of this.pipes) {
+      context.save()
+
+      var w2 = p.w / 2
+      var h2 = p.h / 2
+      context.translate(p.x + w2, p.y + h2)
+      var scaleX = p.flipX ? -1 : 1
+      var scaleY = p.flipY ? -1 : 1
+      context.scale(scaleX, scaleY)
+      context.rotate(p.rotation * Math.PI / 180)
+      context.translate(-w2, -h2)
+      context.drawImage(p.texture, 0, 0)
+
+      context.restore()
+    }
+  }
+}
+
+class SceneTitle extends LinScene {
+  constructor(game) {
+    super(game)
+    // var label = LinLabel.new(game, 'hello from lin')
+    // this.addElements(label)
+
+    // bg
+    var bg = LinImage.new(game, 'bg')
+    this.addElements(bg)
+    // 加入水管
+    this.pipe = Pipes.new(game)
+    this.addElements(this.pipe)
+    // 循环移动的地面
+    this.grounds = []
+    for (var i = 0; i < 30; i++) {
+      var g = LinImage.new(game, 'ground')
+      g.x = i * 19
+      g.y = 400
+      this.addElements(g)
+      this.grounds.push(g)
+    }
+    this.skipCount = 4
+    // bird
+    var b = LinAnimation.new(game)
+    b.x = 150
+    b.y = 180
+    this.bird = b
+    this.addElements(b)
+
+    this.setupInputs()
+  }
+  update() {
+    super.update()
+    // 地面移动
+    this.skipCount--
+    this.offset = -5
+    if (this.skipCount == 0) {
+      this.skipCount = 4
+      this.offset = 15
+    }
+    for (var i = 0; i < 30; i++) {
+      var g = this.grounds[i]
+      g.x += this.offset
+    }
+  }
+  setupInputs() {
+    var self = this
+    var b = this.bird
+    self.game.registerAction('a', function(keyStatus) {
+      b.move(-2, keyStatus)
+    })
+    self.game.registerAction('d', function(keyStatus) {
+      b.move(2, keyStatus)
+    })
+    self.game.registerAction('j', function(keyStatus) {
+      b.jump(2, keyStatus)
+    })
+  }
+}
