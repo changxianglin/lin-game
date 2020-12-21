bcb152dbbb4c5255e48157e664dd7ea14e81521f
changxianglin <281532612@qq.com>
2020-12-22 02:55:53 +0800

    add third game

---
+++ game3/scene/title/scene_title.js
@@ -0,0 +1,14 @@
+class SceneTitle extends LinScene {
+  constructor(game) {
+    super(game)
+    game.registerAction('k', function() {
+      var s = Scene.new(game)
+      game.replaceScene(s)
+    })
+  }
+
+  draw() {
+    // draw lables
+    this.game.context.fillText('按 k 开始游戏', 10, 190)
+  }
+}
